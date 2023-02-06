1b3027c203b0d61ee04bb42c841115cb1ac2d272
yrime <yrime@mail.ru>
2023-02-06 12:35:29 +0300

    add encoding utf-8 cp1251

--- edk2instr.py
+++ edk2instr.py
@@ -17,11 +17,15 @@ if __name__ == '__main__':
         rea = inst.read().lower()
         # print(sys.argv[2], sys.argv[3])
         k = rea.find(instrFile)
-        if rea.find(instrFile) > 0:
-            print(instrFile)
-            a = DPPChecker()
-            instrFile = a.check(instrFile, inc, ccflag)
-            cp.parse(instrFile)
+        ci = instrFile
+        try:
+            if rea.find(instrFile) > 0:
+                print(instrFile)
+                a = DPPChecker()
+                instrFile = a.check(instrFile, inc, ccflag)
+                cp.parse(instrFile)
+        except:
+            instrFile = ci
    # print(runcmd + " " + instrFile)
     subprocess.call(runcmd + " " + instrFile, shell=True)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
